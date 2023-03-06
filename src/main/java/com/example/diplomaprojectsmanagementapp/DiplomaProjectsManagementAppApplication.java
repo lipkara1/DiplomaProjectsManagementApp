f0f6717dc6240dca087bec0774d07e205456c501
giorg <giorgoslipka@gmail.com>
2023-03-06 15:16:03 +0200

    First

---
+++ src/main/java/com/example/diplomaprojectsmanagementapp/DiplomaProjectsManagementAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.diplomaprojectsmanagementapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiplomaProjectsManagementAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiplomaProjectsManagementAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/diplomaprojectsmanagementapp/DiplomaProjectsManagementAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.diplomaprojectsmanagementapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiplomaProjectsManagementAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
