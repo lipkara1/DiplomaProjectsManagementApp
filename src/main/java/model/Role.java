968e5eec835015808aa8347e0c33933572fd2841
giorg <giorgoslipka@gmail.com>
2023-03-11 13:38:53 +0200

    Create User

--- src/main/java/model/Role.java
+++ src/main/java/model/Role.java
@@ -1,4 +1,17 @@
 package model;
 
 public enum Role {
+    STUDENT("Student"),
+    PROFESSOR("Professor");
+
+    private final String value;
+
+    private Role(String value) {
+        this.value = value;
+    }
+
+
+    public String getValue() {
+        return value;
+    }
 }
--- src/main/java/model/Student.java
+++ src/main/java/model/Student.java
@@ -1,6 +1,7 @@
 package model;
 
-import jakarta.persistence.*;
+
+import javax.persistence.*;
 
 @Entity
 @Table(name = "student")
